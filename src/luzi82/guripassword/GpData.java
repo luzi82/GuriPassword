5fcc98dddc62e11862516f121f3e0716fb75add0
luzi82 <luzi82@tiny-ubuntu.(none)>
2009-12-20 10:09:19 +0800

    remove useless random stuff

--- src/luzi82/guripassword/GpData.java
+++ src/luzi82/guripassword/GpData.java
@@ -11,13 +11,7 @@ public class GpData implements Serializable {
 	 */
 	private static final long serialVersionUID = -5958390738812505770L;
 
-	private final GpRandom randomMixer = new GpRandom();
-
 	private final LinkedList<GpEntry> entryList = new LinkedList<GpEntry>();
-
-	public GpRandom getRandomMixer() {
-		return randomMixer;
-	}
 
 	public Collection<GpEntry> getEntryList() {
 		return entryList;
